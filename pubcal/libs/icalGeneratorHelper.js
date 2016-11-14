330e84d866c7893b16bd8eac825611cc70dee258
Alex Nazarkin <anazarkin1@gmail.com>
2016-11-14 17:08:10 -0500

    fix ical format uses `summary` property instead of `name` for events

--- pubcal/libs/icalGeneratorHelper.js
+++ pubcal/libs/icalGeneratorHelper.js
@@ -5,9 +5,10 @@ const path = require('path');
 const fs = require('fs');   //Used to remove files from the disk
 
 exports.createCalendar = function createCalendar(newCal, calPath = null) {
-    //Convert date from strings to Date objects
+    //ical format requries events names' to be called summaries
+    //so we convert
     if (newCal.events.length > 0) {
-        newCal.events.map(convertEvent);
+        newCal.events.map(convertEventName);
     }
 
     if (calPath === null) {
@@ -23,8 +24,8 @@ exports.createCalendar = function createCalendar(newCal, calPath = null) {
 };
 
 exports.deleteCalendar = function deleteCalendar(filepath) {
-    return new Promise((resolve, reject)=> {
-        fs.unlink(filepath, (err)=> {
+    return new Promise((resolve, reject) => {
+        fs.unlink(filepath, (err) => {
             if (err) {
                 reject(err);
             }
@@ -34,25 +35,9 @@ exports.deleteCalendar = function deleteCalendar(filepath) {
     });
 };
 
-function convertEvent(item, index) {
-    let eventDateKeys = ['start', 'end'];
-    for (let key of eventDateKeys) {
-        if (item.key != null)
-            item.key = Date.parse(item.key);
-    }
-
-    if (item.repeating != null && item.repeating.length > 0) {
-        item.repeating.map(convertRepeating);
-    }
-
-};
-
-function convertRepeating(item, index) {
-    let repeatingDateKeys = ['until'];
-    for (let key of repeatingDateKeys) {
-        if (item.key != null) {
-            item.key = Date.parse(item.key);
-        }
+function convertEventName(item, index) {
+    if ("name" in item) {
+        item.summary = item.name;
     }
 };
 
