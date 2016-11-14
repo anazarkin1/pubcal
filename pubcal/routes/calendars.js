8a7d9339de73f617838820abdc26541cbc61b919
Alex Nazarkin <anazarkin1@gmail.com>
2016-11-14 13:40:33 -0500

    fix problem with frontend sending not expected data to POST /calendars/new

--- pubcal/routes/calendars.js
+++ pubcal/routes/calendars.js
@@ -7,11 +7,11 @@ const helper = require('../libs/icalGeneratorHelper');
 
 //Download ical file for a calendar with id = :id
 //GET /calendars/:id/download
-router.get('/:id/download', (req, res)=> {
+router.get('/:id/download', (req, res) => {
     let id = req.params.id;
     let filepath = null;
     let filename = null;
-    CalendarClient.getCalendarById(id).then((calendar)=> {
+    CalendarClient.getCalendarById(id).then((calendar) => {
         filepath = calendar.filepath;
 
         //filename is the last element of splitted array
@@ -27,14 +27,14 @@ router.get('/:id/download', (req, res)=> {
             }
         };
 
-        res.sendFile(filepath, options, (err)=> {
+        res.sendFile(filepath, options, (err) => {
             if (err) {
                 console.log("ERROR: failed to serve calendar file: " + err);
                 res.status(404).send("Error, we couldn't find this calendar");
             }
 
         });
-    }).catch((err)=> {
+    }).catch((err) => {
         console.log("ERROR: failed to serve calendar file: " + err);
         res.send("Error, no such calendar");
     });
@@ -42,24 +42,24 @@ router.get('/:id/download', (req, res)=> {
 
 //Return json object for calendar with this id
 // GET /calendars/:id/json
-router.get('/:id/json', (req, res)=> {
+router.get('/:id/json', (req, res) => {
     let id = req.params.id;
     CalendarClient.getCalendarById(id)
-        .then((calendar)=> {
+        .then((calendar) => {
             res.json(calendar);
         })
-        .catch((err)=> {
+        .catch((err) => {
             if (err)
                 res.json({"status": "failed"});
         });
 });
 
-router.get('/import', (req, res)=> {
+router.get('/import', (req, res) => {
     //TODO: implement
     res.render('calendars/import');
 });
 
-router.get('/search', (req, res)=> {
+router.get('/search', (req, res) => {
     res.render('calendars/search', {results: {}});
 });
 
@@ -80,17 +80,16 @@ router.get('/new', (req, res) => {
 //POST /calendars/new
 router.post('/new', (req, res) => {
     //check if calendar was sent to us
-
-    if (!req.body.hasOwnProperty("calendar")) {
+    if (!("calendar" in req.body)) {
+        console.log("no calendar found")
         res.json({"status": "failed"});
         return;
     }
     let calendar = req.body.calendar;
-    console.log(calendar);
     let createCalendarFile = new Promise((resolve, reject) => {
         let filepath = helper.createCalendar(calendar);
         resolve(filepath);
-    }).then((filepath)=> {
+    }).then((filepath) => {
 
         //Construct list of subscribed users
         calendar.users_subscribed = [];
@@ -140,31 +139,31 @@ router.post('/search', (req, res) => {
 //Update a calendar by id
 //PUT /calendars/:id
 router.put('/:id', (req, res) => {
-    if (!req.body.hasOwnProperty("calendar")) {
+    if (!("calendar" in req.body)) {
         res.json({status: "failed"});
         return;
     }
     let id = req.params.id;
     let calendar = req.body.calendar;
 
-    CalendarClient.getCalendarById(id).then((oldCalendar)=> {
+    CalendarClient.getCalendarById(id).then((oldCalendar) => {
         //Get old calendar, copy its users and filepath, since we want to preserve this information
         //and frontend doesn't send this to us(for security)
         calendar.users_subscribed = oldCalendar.users_subscribed.slice();
         calendar.filepath = oldCalendar.filepath;
         return calendar;
-    }).then((newCalendar)=> {
+    }).then((newCalendar) => {
         //Replace calendar object in the database first, newCalendar contains copied users and filepath
         CalendarClient.replaceCalendar(id, newCalendar)
             .then((result) => {
                 if (result.modifiedCount == 1) {
                     //We are NOT doing upsert, so if modifiedCount >0, means that there was an actual record
                     //that we modified which is good
-                    new Promise((resolve, reject)=> {
+                    new Promise((resolve, reject) => {
                         //We want to create a new ical file on the disk with the same filepath(filename) as
                         let filepath = helper.createCalendar(newCalendar, newCalendar.filepath);
                         resolve(filepath);
-                    }).then((filepath)=> {
+                    }).then((filepath) => {
                         if (filepath === newCalendar.filepath)
                             res.json({"status": "success", "id": id});
                         else {
@@ -172,7 +171,7 @@ router.put('/:id', (req, res) => {
                             console.log("ERROR: Promise returned a different filepath than expected");
                             res.json({"status": "failed"});
                         }
-                    }).catch((err)=> {
+                    }).catch((err) => {
                         console.log("ERROR: Failed on promise to update file: " + err);
                         res.json({"status": "failed"});
                     });
@@ -188,11 +187,11 @@ router.put('/:id', (req, res) => {
 
 //Get particular by id
 //Get /calendars/:id
-router.get('/:id', (req, res)=> {
+router.get('/:id', (req, res) => {
     let id = req.params.id;
 
     //TODO: WE don't want to return ALL fields of calendar object(ie user doesn't need to know filepath, user_subscribed etc...)
-    CalendarClient.getCalendarById(id).then((calendar)=> {
+    CalendarClient.getCalendarById(id).then((calendar) => {
         console.log(calendar);
         res.render('calendar', calendar);
     });
@@ -205,9 +204,9 @@ router.delete('/:id', (req, res) => {
     //Keep old copy of calendar in case we need to restore it
     //For example: we removed database object, but disk file failed to get removed and we want to revert everything back
     let tmpCalendar;
-    CalendarClient.getCalendarById(id).then((calendar)=> {
+    CalendarClient.getCalendarById(id).then((calendar) => {
         tmpCalendar = calendar;
-    }).then(()=> {
+    }).then(() => {
 
         //Remove database object
         CalendarClient.removeCalendarById(id)
@@ -216,11 +215,11 @@ router.delete('/:id', (req, res) => {
                     //Successful delete inside db
                     //Do file delete
                     helper.deleteCalendar(tmpCalendar.filepath)
-                        .then((result)=> {
+                        .then((result) => {
                             //Successfully removed file
                             res.json({"status": "success"});
                         })
-                        .catch((err)=> {
+                        .catch((err) => {
                             console.log("ERROR: failed to remove calendar file: " + err);
                             res.json({"status": "failed"});
                         });
@@ -229,7 +228,7 @@ router.delete('/:id', (req, res) => {
                     res.json({"status": "failed"});
                 }
             })
-            .catch((err)=> {
+            .catch((err) => {
                 console.log("ERROR: Failed to delete calendar from database");
                 res.json({"status": "failed"});
             });
@@ -240,7 +239,7 @@ router.delete('/:id', (req, res) => {
 
 //Import ical file
 //POST /calendars/import
-router.post('/import', (req, res)=> {
+router.post('/import', (req, res) => {
     //TODO: implement
     res.send("not implemented");
 });
