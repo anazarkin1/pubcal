48b58f3121588735cbe578765baf2d3430b9a178
njia95 <nathan.jia@mail.utoronto.ca>
2016-11-14 18:05:01 -0500

    refactored iClaHelper

--- pubcal/libs/icalGeneratorHelper.js
+++ pubcal/libs/icalGeneratorHelper.js
@@ -1,38 +1,8 @@
 "use strict";
-const ical = require('ical-generator'); //Used to generate ICAL files
+const iCal = require('ical-generator'); //Used to generate ICAL files
 const uuid = require('uuid');   //Used to generate random names for calendars
 const path = require('path');
 const fs = require('fs');   //Used to remove files from the disk
-
-exports.createCalendar = function createCalendar(newCal, calPath = null) {
-    //Convert date from strings to Date objects
-    if (newCal.events.length > 0) {
-        newCal.events.map(convertEvent);
-    }
-
-    if (calPath === null) {
-        let newName = uuid.v4().substr(0, 7);
-        calPath = path.join(__dirname, '../calendars/' + newName + '.ics');
-    }
-
-    let cal = ical(newCal);
-    //Save to directory with all calendars
-    cal.save(calPath);
-    return calPath;
-
-};
-
-exports.deleteCalendar = function deleteCalendar(filepath) {
-    return new Promise((resolve, reject)=> {
-        fs.unlink(filepath, (err)=> {
-            if (err) {
-                reject(err);
-            }
-            resolve(filepath);
-        });
-
-    });
-};
 
 function convertEvent(item, index) {
     let eventDateKeys = ['start', 'end'];
@@ -40,12 +10,10 @@ function convertEvent(item, index) {
         if (item.key != null)
             item.key = Date.parse(item.key);
     }
-
     if (item.repeating != null && item.repeating.length > 0) {
         item.repeating.map(convertRepeating);
     }
-
-};
+}
 
 function convertRepeating(item, index) {
     let repeatingDateKeys = ['until'];
@@ -54,5 +22,34 @@ function convertRepeating(item, index) {
             item.key = Date.parse(item.key);
         }
     }
-};
+}
 
+class Helper {
+    static createCalendar(newCal, calPath=null) {
+        //Convert date from strings to Date objects
+        if (newCal.events.length > 0) {
+            newCal.events.map(convertEvent);
+        }
+        if (calPath === null) {
+            let newName = uuid.v4().substr(0, 7);
+            calPath = path.join(__dirname, '../calendars/' + newName + '.ics');
+        }
+        let cal = iCal(newCal);
+        //Save to directory with all calendars
+        cal.save(calPath);
+        return calPath;
+    }
+
+    static deleteCalendar(filePath) {
+        return new Promise((resolve, reject) => {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(filePath);
+            });
+        });
+    }
+}
+
+module.exports = Helper;
