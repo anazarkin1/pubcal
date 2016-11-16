4c7dac5bf9e2c2eb800243373b8771a0c3932339
Barry Yung <barry.yung@mail.utoronto.ca>
2016-11-15 20:18:48 -0500

    changed the login to popup

--- pubcal/public/js/login.js
+++ pubcal/public/js/login.js
@@ -1,12 +1,33 @@
-$(document).ready(() => {
-    $('#loginButton').click((event) => {
-        $('#popular').hide();
-        $('#popup').show();
-    });
+// $(document).ready(() => {
+//     $('#loginButton').click((event) => {
+//         $('#popular').hide();
+//         $('#popup').show();
+//     });
+//
+//     $('#close').click((event) => {
+//         $('#popup').hide();
+//         $('#popular').show();
+//     });
+//
+//     $('#signup').click((event) => {
+//         $('#loginForm').hide();
+//         $('#signupForm').show();
+//     });
+//
+//     $('#login').click((event) => {
+//         $('#signupForm').hide();
+//         $('#loginForm').show();
+//     });
+// });
+$(document).ready(function() {
+    // $('#loginButton').click((event) => {
+    //     $('#popular').hide();
+    //     $('#popup').show();
+    // });
 
     $('#close').click((event) => {
-        $('#popup').hide();
-        $('#popular').show();
+        $('#mask').hide();
+        $('#login-box').hide();
     });
 
     $('#signup').click((event) => {
@@ -18,4 +39,31 @@ $(document).ready(() => {
         $('#signupForm').hide();
         $('#loginForm').show();
     });
+
+    $('a.login-window').click(function() {
+
+                //Getting the variable's value from a link
+        var loginBox = $(this).attr('href');
+
+        //Fade in the Popup
+        $(loginBox).fadeIn(300);
+
+        //Set the center alignment padding + border see css style
+        var popMargTop = ($(loginBox).height() + 24) / 2;
+        var popMargLeft = ($(loginBox).width() + 24) / 2;
+
+        $(loginBox).css({
+            'margin-top' : -popMargTop,
+            'margin-left' : -popMargLeft
+        });
+
+        // Add the mask to body
+        $('body').append('<div id="mask"></div>');
+        $('#mask').fadeIn(300);
+
+        return false;
+    });
+
+    // When clicking on the button close or the mask layer the popup closed
+
 });
