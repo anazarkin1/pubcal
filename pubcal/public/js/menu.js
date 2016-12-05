b0d8e63544595dadec1d472259d1f9e946c562d9
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-12-05 17:24:13 -0500

    Merge branch 'master' of /Users/sangjunahn/Desktop/301t/project-team-18 with conflicts.

---
+++ pubcal/public/js/menu.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $('#close').click((event) => {
+        $('#mask').hide();
+    $('#menu-box').hide();
+});
+
+    $(document).keydown((e) => {
+        if (e.keyCode == 27) {
+        $('#mask').hide();
+        $('#menu-box').hide();
+    }
+});
+
+    $("body").click(() => {
+        $('#mask').hide();
+    $('#menu-box').hide();
+});
+
+    $('#menu-box').click((e) => {
+        e.stopPropagation();
+})
+
+    $('#search-value').click(()=>{
+        $("#search-value").val("");
+    });
+
+    $('a#menu-window').click(function() {
+
+        //Getting the variable's value from a link
+        var loginBox = $(this).attr('href');
+
+        //Fade in the Popup
+        $(loginBox).fadeIn(300);
+
+        //Set the center alignment padding + border see css style
+        var popMargTop = ($(loginBox).height() + 24) / 2;
+        var popMargLeft = ($(loginBox).width() + 24) / 2;
+
+        $(loginBox).css({
+            'margin-top' : -popMargTop,
+            'margin-left' : -popMargLeft
+        });
+
+        // Add the mask to body
+        $('body').append('<div id="mask"></div>');
+        $("#search-value").val("Type here...");
+        $('#mask').fadeIn(300);
+        $("#pop").val("hahaha");
+
+        return false;
+    });
+});
+
