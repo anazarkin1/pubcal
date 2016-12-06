5a7df5c4e989ec3956d17f1b07749d9acea68cbe
Alex Nazarkin <anazarkin1@gmail.com>
2016-12-05 19:18:14 -0500

    fix #38

--- pubcal/public/js/sub-unsub.js
+++ pubcal/public/js/sub-unsub.js
@@ -1,56 +1,57 @@
-$(document).ready(function() {
-	var username = $('#username').text();
-	// var subscribed = $('#subscribed').text();
-	var subscribed = 'true';
-	var calID = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
+"use strict";
+$(document).ready(function () {
+    var username = $('#username').text();
+    var subscribed = $('#subscribed').text();
+    var calID = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
 
-	if (subscribed == 'true') {
-		$('#unsubscribe-btn').show();
-		$('#subscribe-btn').hide();
+    if (subscribed == 'true') {
+        $('#unsubscribe-btn').show();
+        $('#subscribe-btn').hide();
 
-	}
-	else{
-		$('#unsubscribe-btn').hide();
-		$('#subscribe-btn').show();		
-	};
+    }
+    else {
+        $('#unsubscribe-btn').hide();
+        $('#subscribe-btn').show();
+    }
+    ;
 
-	$('#subscribe-btn').click(function(e){
-		e.preventDefault();
-		// Get the calendar id
-		// Do some ajax here
-		var url = "../../users/subscribe/" + calID;
-		$.ajax({
-			url: url,
-			type: 'POST',
-			data: {
-				username: username
-			},
-			dataType: "json",
-			contentType: 'application/json'
-		});
-
-
-
-		$('#unsubscribe-btn').show();
-		$('#subscribe-btn').hide();
-	});
+    $('#subscribe-btn').click(function (e) {
+        e.preventDefault();
+        let url = "/users/subscribe/" + calID;
+        let data = JSON.stringify({"username": username});
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: (data) => {
+                $('#unsubscribe-btn').show();
+                $('#subscribe-btn').hide();
+            }
+        });
 
 
-	$('#unsubscribe-btn').click(function(e){
-		e.preventDefault();
-		var url = "../../users/unsubscribe/" + calID;
-		$.ajax({
-			url: url,
-			type: 'POST',
-			data: {
-				username: username
-			},
-			dataType: "json",
-			contentType: 'application/json'
-		});
-		$('#unsubscribe-btn').hide();
-		$('#subscribe-btn').show();
-	});
+    });
+
+
+    $('#unsubscribe-btn').click(function (e) {
+        e.preventDefault();
+        let url = "/users/unsubscribe/" + calID;
+        let data = JSON.stringify({"username": username});
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $('#unsubscribe-btn').hide();
+                $('#subscribe-btn').show();
+
+            }
+        });
+    });
 
 
 });
